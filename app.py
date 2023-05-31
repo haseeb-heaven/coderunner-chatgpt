d0cc4292291d33e120d159b95cae8cef1c7658b6
HeavenHM <haseeb_heaven@yahoo.com>
2023-05-31 19:42:31 +0000

    Added Exception Handling

--- app.py
+++ app.py
@@ -74,24 +74,29 @@ lang_codes = {
 
 #Method to configure logs.
 def configure_logger(name: str, filename: str):
-  logger = logging.getLogger(name)
-  logger.setLevel(logging.INFO)
+  try:
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.INFO)
 
-  file_handler = logging.FileHandler(filename)
-  formatter = logging.Formatter(
-    '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-  file_handler.setFormatter(formatter)
+    file_handler = logging.FileHandler(filename)
+    formatter = logging.Formatter(
+      '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    file_handler.setFormatter(formatter)
 
-  logger.addHandler(file_handler)
+    logger.addHandler(file_handler)
+  except Exception as e:
+    print(e)
   return logger
 
 
 def generate_code_id(response, length=10):
-  characters = string.ascii_letters + string.digits
-  unique_id = ''.join(random.choice(characters) for i in range(length))
-  response['id'] = unique_id
+  try:
+    characters = string.ascii_letters + string.digits
+    unique_id = ''.join(random.choice(characters) for i in range(length))
+    response['id'] = unique_id
+  except Exception as e:
+    print(e)
   return response
-
 
 # Method to get the JDoodle client ID and secret.
 def get_jdoodle_client_1():
@@ -112,71 +117,87 @@ def get_jdoodle_client_2():
 
 
 def get_jdoodle_client():
-  index = 1
-  logger.info(f"get_jdoodle_client: Getting jdoodle client {index}")
-  credits_used = get_credits_used()
-  if credits_used < 200:
-    logger.info("get_jdoodle_client: return client_1")
-    return get_jdoodle_client_1()
-  else:
-    logger.error("Credits exhaused for client_1")
-    logger.info("get_jdoodle_client: return client_2")
+  try:
+    index = 1
+    logger.info(f"get_jdoodle_client: Getting jdoodle client {index}")
+    credits_used = get_credits_used()
+    if credits_used < 200:
+      logger.info("get_jdoodle_client: return client_1")
+      return get_jdoodle_client_1()
+    else:
+      logger.error("Credits exhaused for client_1")
+      logger.info("get_jdoodle_client: return client_2")
+  except Exception as e:
+    logger.error(f"get_jdoodle_client: {e}")
     return get_jdoodle_client_2()
 
 
 # Method to call the JDoodle "credit-spent" API.
 def get_jdoodle_credit_spent():
-  client_id, client_secret = get_jdoodle_client_1()
-  url = "https://api.jdoodle.com/v1/credit-spent"
-  headers = {
-    'Content-Type': 'application/json',
-    'X-Requested-With': 'XMLHttpRequest',
-  }
+  try:
+    client_id, client_secret = get_jdoodle_client_1()
+    url = "https://api.jdoodle.com/v1/credit-spent"
+    headers = {
+      'Content-Type': 'application/json',
+      'X-Requested-With': 'XMLHttpRequest',
+    }
 
-  body = {"clientId": client_id, "clientSecret": client_secret}
-  logger.info(f"get_jdoodle_credit_spent: sending request with url {url}")
-  credit_spent = requests.post(url, headers=headers, data=json.dumps(body))
-  logger.info(f"get_jdoodle_credit_spent: {credit_spent}")
+    body = {"clientId": client_id, "clientSecret": client_secret}
+    logger.info(f"get_jdoodle_credit_spent: sending request with url {url}")
+    credit_spent = requests.post(url, headers=headers, data=json.dumps(body))
+    logger.info(f"get_jdoodle_credit_spent: {credit_spent}")
+  except Exception as e:
+    logger.error(f"get_jdoodle_credit_spent: {e}")
   return credit_spent
 
 
 # Helper method to get the request.
 def get_request():
-  return request_var.get()
+  try:
+    return request_var.get()
+  except Exception as e:
+    print(f"get_request: {e}")
 
 
 def set_request(request: Request):
-  request_var.set(request)
+  try:
+    request_var.set(request)
+  except Exception as e:
+    print(f"set_request: {e}")
 
 
 @app.middleware("http")
 async def set_request_middleware(request: Request, call_next):
-  set_request(request)
-  response = await call_next(request)
-  return response
+  try:
+    set_request(request)
+    response = await call_next(request)
+    return response
+  except Exception as e:
+    print(f"set_request_middleware: {e}")
+  return None
 
 
 # Method to run the code.
 @app.post('/run_code')
 async def run_code():
-  request = get_request()
-  data = await request.json()
-  logger.info(f"run_code: data is {data}")
-  script = data.get('script')
-  language = data.get('language')
-
-  # Convert the language to the JDoodle language code.
-  language_code = lang_codes.get(language, language)
-  logger.info(f"run_code: language_code is {language_code}")
-
-  # Declare input and compileOnly optional.
-  input = data.get('input', None)
-  compile_only = data.get('compileOnly', False)
-
   try:
+    request = get_request()
+    data = await request.json()
+    logger.info(f"run_code: data is {data}")
+    script = data.get('script')
+    language = data.get('language')
+
+    # Convert the language to the JDoodle language code.
+    language_code = lang_codes.get(language, language)
+    logger.info(f"run_code: language_code is {language_code}")
+
+    # Declare input and compileOnly optional.
+    input = data.get('input', None)
+    compile_only = data.get('compileOnly', False)
+
     # Get the JDoodle client ID and secret.
     client_id, client_secret = get_jdoodle_client()
-    compiler_api_Url = "https://api.jdoodle.com/v1/execute"
+    compiler_url = "https://api.jdoodle.com/v1/execute"
     headers = {
       'Content-Type': 'application/json',
       'X-Requested-With': 'XMLHttpRequest',
@@ -198,10 +219,9 @@ async def run_code():
     }
 
     logger.info(f"run_code: body is {body_filtered}")
-    response_data = requests.post(compiler_api_Url,
-                                  headers=headers,
-                                  data=json.dumps(body))
+    response_data = requests.post(compiler_url,headers=headers,data=json.dumps(body))
     response = json.loads(response_data.content.decode('utf-8'))
+
     # Check reponse status code before appending the code id.
     if response_data.status_code == 200:
       response = generate_code_id(response)
@@ -215,61 +235,78 @@ async def run_code():
 # Method to save the code.
 @app.post('/save_code')
 async def save_code():
-  request = get_request()
-  data = await request.json()  # Get JSON data from request
-  logger.info(f"save_code: data is {data}")
-  filename = data.get('filename')
-  code = data.get('code')
+  try:
+    request = get_request()
+    data = await request.json()  # Get JSON data from request
+    logger.info(f"save_code: data is {data}")
+    filename = data.get('filename')
+    code = data.get('code')
 
-  if filename is None or code is None:
-    return {"error": "filename or code not provided"}, 400
+    if filename is None or code is None:
+      return {"error": "filename or code not provided"}, 400
 
-  directory = 'codes'
-  filepath = os.path.join(directory, filename)
+    directory = 'codes'
+    filepath = os.path.join(directory, filename)
 
-  logger.info(f"save_code: filename is {filepath} and code was present")
-  async with aiofiles.open(filepath, 'w') as f:
-    await f.write(code)
-  logger.info(f"save_code: wrote code to file {filepath}")
-  download_link = f'{request.url_for("download",filename=filename)}'
-  logger.info(f"save_code: download link is {download_link}")
-  output = ""
-  if download_link:
-    output = {"download_link": download_link}
+    logger.info(f"save_code: filename is {filepath} and code was present")
+    async with aiofiles.open(filepath, 'w') as f:
+      await f.write(code)
+    logger.info(f"save_code: wrote code to file {filepath}")
+    download_link = f'{request.url_for("download",filename=filename)}'
+    logger.info(f"save_code: download link is {download_link}")
+    output = ""
+    if download_link:
+      output = {"download_link": download_link}
+  except Exception as e:
+    logger.error(f"save_code: {e}")
   return output
 
 
 # Method to download the file.
 @app.get('/download/{filename}')
 async def download(filename: str):
-  directory = 'codes'
-  filepath = os.path.join(directory, filename)
-  logger.info(f"download filename is {filepath}")
-  return FileResponse(filepath, filename=filepath)
+  try:
+    directory = 'codes'
+    filepath = os.path.join(directory, filename)
+    logger.info(f"download filename is {filepath}")
+    return FileResponse(filepath, filename=filepath)
+  except Exception as e:
+    logger.error(f"download: {e}")
 
 
 # Plugin logo.
 @app.get("/logo.png")
 async def plugin_logo():
-  filename = 'logo.png'
-  logging.info(f"logo filename is {filename}")
+  try:
+    filename = 'logo.png'
+    logging.info(f"logo filename is {filename}")
+  except Exception as e:
+    logger.error(f"plugin_logo: {e}")
   return FileResponse(filename)
 
 
 # Plugin manifest.
 @app.get("/.well-known/ai-plugin.json")
 async def plugin_manifest():
-  with open("./.well-known/ai-plugin.json") as f:
-    text = f.read()
-    return Response(text, media_type="text/json")
+  try:
+    text = ""
+    with open("./.well-known/ai-plugin.json") as f:
+      text = f.read()
+  except Exception as e:
+    logger.error(f"plugin_manifest: {e}")
+  return Response(text, media_type="text/json")
 
 
 # Plugin OpenAPI spec.
 @app.get("/openapi.yaml")
 async def openapi_spec():
-  with open("openapi.yaml") as f:
-    text = f.read()
-    return Response(text, media_type="text/yaml")
+  try:
+    text = ""
+    with open("openapi.yaml") as f:
+      text = f.read()
+  except Exception as e:
+    logger.error(f"openapi_spec: {e}")
+  return Response(text, media_type="text/yaml")
 
 
 def get_credits_used():
@@ -295,10 +332,8 @@ def show_credits_spent():
   try:
     credits_used = get_credits_used()
     return {"credits:": credits_used}
-
   except Exception as e:
     return {"error": str(e)}
-    return {"result": credit_spent}
 
 
 @app.get('/help')
@@ -344,13 +379,12 @@ async def help():
   }
   return json_data
 
-
-# Testing purpose.
-# call this with uvicorn main:app --reload only.
-logger = configure_logger('CodeRunner', 'CodeRunner.log')
-
 # Run the app.
 # Will only work with python main.py
 if __name__ == "__main__":
-  logger = configure_logger('CodeRunner', 'CodeRunner.log')
-  uvicorn.run("app:app")
+  try:
+    logger = configure_logger('CodeRunner', 'CodeRunner.log')
+    uvicorn.run("app:app",reload=True)
+  except Exception as e:
+    print(str(e))
+
