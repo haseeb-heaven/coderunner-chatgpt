60c723d16c3c42ce40e294a90b25cbec06c8b5b3
HeavenHM <haseeb_heaven@yahoo.com>
2023-05-31 17:05:52 +0000

    Fixed App run

--- app.py
+++ app.py
@@ -353,4 +353,4 @@ logger = configure_logger('CodeRunner', 'CodeRunner.log')
 # Will only work with python main.py
 if __name__ == "__main__":
   logger = configure_logger('CodeRunner', 'CodeRunner.log')
-  uvicorn.run(app, debug=True, reload=True) #Start the server with Debug and Reload flags.
+  uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
