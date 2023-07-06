5aba8d11ca44dc8a7b6f81808fc11849e6c8833f
Haseeb <haseeb_heaven@yahoo.com>
2023-07-07 03:35:55 +0530

    Fixed TinyURL for upload

--- script.py
+++ script.py
@@ -177,7 +177,6 @@ async def run_code():
 
                         if response.__len__() == 0 and contains_graph:
                             download_link = f"{plugin_url}/download/{graph_file}"
-                            download_link = generate_tinyurl(download_link)
                             response = {"output": download_link}
                             
                             # obsolete support message for Graphical libraries.
@@ -626,7 +625,7 @@ async def create_quickchart():
         write_log(f"quick_chart: generated chart successfully")
         
         download_link = quick_chart.download_link(graph_file)
-        download_link = generate_tinyurl(download_link)
+        #download_link = generate_tinyurl(download_link)
         
         # Return a success message and status code
         response = {"link": download_link}
