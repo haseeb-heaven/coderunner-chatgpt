f6e6421b76f170b57a732f74cecf9c9c07336555
Haseeb <haseeb_heaven@yahoo.com>
2023-07-09 10:24:46 +0530

    Updated execute_code

--- lib/python_runner.py
+++ lib/python_runner.py
@@ -1,25 +1,8 @@
-import random
-import string
-import os
 import io
 import contextlib
-import multiprocessing
-
-# setting default timeout to 120 seconds
-DEFAULT_TIMEOUT = 120
-
-def execute_code(code, timeout=DEFAULT_TIMEOUT):
-    with multiprocessing.Pool(processes=1) as pool:
-        res = pool.apply_async(_execute_code, (code,)) # runs in *only* one process
-        try:
-            output = res.get(timeout=timeout) # waits for the result for `timeout` seconds
-        except multiprocessing.TimeoutError:
-            output = "Code execution took too long and was terminated."
-            pool.terminate()
-    return output
 
 # Method to execute the code using exec.
-def _execute_code(code):
+def execute_code(code):
     # Create a string buffer to store the output
     buffer = io.StringIO()
     # Redirect the standard output to the buffer
